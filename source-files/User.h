9373e8763ed3f186f80b2515e5e1dbe06f77d64b
Yağız ARAS <ygzars0@gmail.com>
2025-12-06 16:27:26 +0300

    "unnecessary changes"

--- source-files/User.h
+++ source-files/User.h
@@ -1,9 +1,25 @@
-#pragma once
-#include <iostream>
-using namespace std;
-class User {
-protected:
+#ifndef USER_H_INCLUDED
+#define USER_H_INCLUDED
+
+class user {
+private:
+    int* userID;
+    char* email;
+
+protected: 
+    char* name;
 
 public:
+    user(const int* userNo, const char* userName, const char* userEmail);
+    
+    const int* getuserID() const;
+    const char* getname() const;
+    const char* getemail() const;
+
+    virtual void displayInfo() const = 0;
+    virtual ~user();
 
 };
+
+
+#endif 
